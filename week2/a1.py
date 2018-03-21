96dae5fe63ec9b5e3393a72d5e0b6c270493ace6
Joseph Calvo <josephcalvo@Calvo-Comp-3.local>
2018-03-21 15:46:42 -0700

    Added a 0 to example

--- week2/a1.py
+++ week2/a1.py
@@ -5,7 +5,7 @@ def seconds_difference(time_1, time_2):
     time_2 is than a time in seconds time_1.
         
     >>> seconds_difference(1800.0, 3600.0)
-    1800.0
+    1800.00
     >>> seconds_difference(3600.0, 1800.0)
     -1800.0
     >>> seconds_difference(1800.0, 2160.0)
